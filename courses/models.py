6f58559f67211f8128707b969be7b0ece9fd662e
John Gaitho <johngaitho05@gmail.com>
2024-05-13 15:33:56 +0300

    define course and content models

--- courses/models.py
+++ courses/models.py
@@ -8,3 +8,20 @@ class Course(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=255, blank=False, null=False)
+    course = models.ForeignKey(Course, null=False, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class ContentLink(models.Model):
+    lesson = models.ForeignKey(Lesson, null=False, on_delete=models.CASCADE, related_name='content')
+    title = models.CharField(max_length=255, null=False, blank=False)
+    href = models.URLField(null=False, blank=False)
+
+    def __str__(self):
+        return self.title
